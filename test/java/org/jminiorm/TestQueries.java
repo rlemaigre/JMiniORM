542e066a3af16fc77f999ee6c791408d62a1373a
rlemaigre <rlemaigre.github@gmail.com>
2018-01-10 15:15:48 +0100

    -

--- test/java/org/jminiorm/TestQueries.java
+++ test/java/org/jminiorm/TestQueries.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +19,13 @@ public class TestQueries {
 
     private static Server tcpServer;
 
+    /**
+     * This is not mandatory to use in memory H2 databases, but it makes it possible to set a breakpoint (that must only
+     * interrupt the current thread, not all of them, otherwise the server becomes unresponsive !) and inspect the in
+     * memory tables from an external JDBC client. Useful for debugging.
+     *
+     * @throws Exception
+     */
     @BeforeAll
     public static void startTCPServer() throws Exception {
         tcpServer = Server.createTcpServer();
@@ -36,12 +44,12 @@ public class TestQueries {
 
         // Table creation :
         db.createTable(Bean.class);
-        Map<String, Object> map = db.select("show tables").one();
+        Map<String, Object> map = db.select("show tables").asMap().one();
         assertEquals("BEANS", map.get("table_name"));
-        List<Map<String, Object>> list = db.select("show columns from beans").list();
+        List<Map<String, Object>> list = db.select("show columns from beans").asMap().list();
         assertEquals(8, list.size());
 
-        // Insertion and select :
+        // Insertion and reselect :
         Bean b1 = new Bean();
         b1.setLocalDate(LocalDate.now());
         b1.setLocalDateTime(LocalDateTime.now());
@@ -64,6 +72,35 @@ public class TestQueries {
         // Delete :
         db.delete(b3);
         assertEquals(0, db.select(Bean.class).list().size());
+
+        // Generic selects :
+        db.sql("truncate table beans");
+        db.insert(Arrays.asList(
+                new Bean("b1"),
+                new Bean("b2"),
+                new Bean("b3")
+                )
+        );
+        Long countLong = db.select("select count(*) from beans").asPrimitive(Long.class).one();
+        assertEquals(new Long(3), countLong);
+        Integer countInteger = db.select("select count(*) from beans").asPrimitive(Integer.class).one();
+        assertEquals(new Integer(3), countInteger);
+        String countString = db.select("select count(*) from beans").asPrimitive(String.class).one();
+        assertEquals("3", countString);
+        Map<String, Map<String, Object>> resultAsIndexedMaps = db.select("select * from beans").asMap().index
+                ("short_text");
+        assertEquals(3, resultAsIndexedMaps.size());
+        assertEquals("b1", resultAsIndexedMaps.get("b1").get("short_text"));
+        List<Pojo> pojos = db.select("show columns from beans").asObject(Pojo.class).list();
+        assertEquals(8, pojos.size());
+        Map<String, List<Pojo>> groups = db.select("show columns from beans").asObject(Pojo.class).group("field");
+        assertEquals(8, groups.size());
+        assertEquals("VARBINARY(2147483647)", groups.get("BYTES").get(0).getType());
+
+        // ORM select :
+        Map<String, Bean> resultAsIndexedObject = db.select(Bean.class).index("shortText");
+        assertEquals(3, resultAsIndexedObject.size());
+        assertEquals("b1", resultAsIndexedObject.get("b1").getShortText());
     }
 
 }
