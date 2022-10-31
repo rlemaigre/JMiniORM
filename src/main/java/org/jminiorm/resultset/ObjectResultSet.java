cd257ebe75132818200a68bcb13478819973bf2b
Régis Lemaigre <rlemaigre@uliege.be>
2022-10-31 12:48:31 +0100

    fix bug

--- src/main/java/org/jminiorm/resultset/ObjectResultSet.java
+++ src/main/java/org/jminiorm/resultset/ObjectResultSet.java
@@ -30,8 +30,7 @@ public class ObjectResultSet<T> extends AbstractResultSet<T> implements IObjectR
             for (Class<?> parameterType : constructor.getParameterTypes()) {
                 args.add(canonicalValue(parameterType));
             }
-            constructor.newInstance(args.toArray());
-            T obj = targetClass.newInstance();
+            T obj = (T) constructor.newInstance(args.toArray());
             for (ColumnMapping columnMapping : getMapping().getColumnMappings()) {
                 columnMapping.writeProperty(obj, row.get(columnMapping.getColumn()));
             }
